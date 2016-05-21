b287f19a505b0c49adbdbe45b074db79be2c527b
Steven Roland <steven@mojave.io>
2016-05-21 13:21:56 -0400

    Refactor
    - Switched specific type names with var since they weren't neede.
    - Removed keepGoing variable, replaced with a break statement.
    - Shortened runningTotal expression with +=.

--- csharp-console-fitnesstracker/Program.cs
+++ csharp-console-fitnesstracker/Program.cs
@@ -10,59 +10,55 @@ namespace csharp_console_fitnesstracker
     {
         static void Main()
         {
-            double runningTotal = 0;
-            bool keepGoing = true;
+            var runningTotal = 0.0;
 
             // ------ Repeat until the user quits
-            while (keepGoing)
+            while (true)
             {
                 // ------ Prompt user for minutes exercised
                 System.Console.WriteLine("How many minutes did you exercise? Type \"quit\" to exit...");
-                string userInput = System.Console.ReadLine();
+                var userInput = System.Console.ReadLine();
                 if (userInput.ToLower() == "quit")
                 {
-                    keepGoing = false;
+                    break;
                 }
-                else
+                try
                 {
-                    try
+                    var minutes = double.Parse(userInput);
+
+                    // ------ Give some feedback to the user based on the number of minutes entered
+                    if (minutes <= 0)
                     {
-                        double minutes = double.Parse(userInput);
-
-                        // ------ Give some feedback to the user based on the number of minutes entered
-                        if (minutes <= 0)
-                        {
-                            System.Console.WriteLine("Well what are you waiting for? Let's get in shape!");
-                            continue;
-                        }
-                        else if (minutes <= 10)
-                        {
-                            System.Console.WriteLine("Don't stop now, you've got this!");
-                        }
-                        else if (minutes <= 30)
-                        {
-                            System.Console.WriteLine("Awesome job!");
-                        }
-                        else if (minutes <= 60)
-                        {
-                            System.Console.WriteLine("Whoa! That's amazing!");
-                        }
-                        else
-                        {
-                            System.Console.WriteLine("Very impressive!");
-                        }
-
-                        // ------ Add minutes exercised to running total
-                        runningTotal = runningTotal + minutes;
-
-                        // ------ Display total minutes exercised on the screen
-                        System.Console.WriteLine("You've exercised for " + runningTotal + " minutes.");
-                    }
-                    catch (FormatException)
-                    {
-                        System.Console.WriteLine("Invalid input.");
+                        System.Console.WriteLine("Well what are you waiting for? Let's get in shape!");
                         continue;
                     }
+                    else if (minutes <= 10)
+                    {
+                        System.Console.WriteLine("Don't stop now, you've got this!");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        System.Console.WriteLine("Awesome job!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        System.Console.WriteLine("Whoa! That's amazing!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Very impressive!");
+                    }
+
+                    // ------ Add minutes exercised to running total
+                    runningTotal += minutes;
+
+                    // ------ Display total minutes exercised on the screen
+                    System.Console.WriteLine("You've exercised for " + runningTotal + " minutes.");
+                }
+                catch (FormatException)
+                {
+                    System.Console.WriteLine("Invalid input.");
+                    continue;
                 }
             }
             System.Console.WriteLine("Goodbye");
